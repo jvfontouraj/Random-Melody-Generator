a6841aef15af907c4af56eded3220d8d57c4e7a6
João Vitor Fontoura <jvfontouraj@gmail.com>
2022-03-31 11:22:14 -0300

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+- Gerador de sequência melódica
+	(escolher a escala ou cromática)
+	(Selecionar a quantidade de notas)
+
+.Input 1 -> Nota de referência (Fundamental) ou escala cromática
+	Notas de referência = Escala Cromática, C, Cm, D, Dm, E, Em, F, Fm, G, Gm, A, Am, B, Bm
+
+.Input 2 -> Escala Maior ou Menor (se cromática, desativar input)
+
+.Input 3 -> Quantidade de notas (até 20)
+	Notas = C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+
---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+let numbersScale = []
+let melodyScale = []
+let melody = ""
+
+const refNote = document.querySelector('.refnote').value
+const scale = document.querySelector('.scale').value
+const numberNotes = Number(document.querySelector('.number').value)
+
+
+document.querySelector('.find').addEventListener('click', function(){
+    const refNote = document.querySelector('.refnote').value
+    const refNoteUper = refNote.toUpperCase()
+
+    const scale = document.querySelector('.scale').value
+    const numberNotes = Number(document.querySelector('.number').value)
+    
+    document.querySelector('.message').textContent = `Generating a melody in ${refNoteUper} ${scale} with ${numberNotes} notes`
+
+    const refNoteIndex = Number(notes.indexOf(refNoteUper))
+    function setMelodyScale(){
+        
+        melodyScale.push(notes[numbersScale[0]], notes[numbersScale[1]], notes[numbersScale[2]], notes[numbersScale[3]], notes[numbersScale[4]], notes[numbersScale[5]], notes[numbersScale[6]])
+    }
+
+    //hidde "scale" input if "Chromatic Scale" is on "refnote"
+
+    if(refNote === 'Chromatic Scale'){
+        melodyScale = notes;
+        console.log(melodyScale)
+    } else{
+        if(scale === "Major"){
+            numbersScale.push(refNoteIndex, refNoteIndex+2, refNoteIndex+4, refNoteIndex+5, refNoteIndex+7, refNoteIndex+9, refNoteIndex+11)
+            setMelodyScale()
+            console.log(melodyScale)
+        } else if(scale === "Minor"){
+            numbersScale.push(refNoteIndex, refNoteIndex+2, refNoteIndex+3, refNoteIndex+5, refNoteIndex+7, refNoteIndex+8, refNoteIndex+10)
+            setMelodyScale()
+            console.log(melodyScale)
+        }
+    }
+
+    for(let i=0; i<numberNotes; i++){
+        let noteNumber = Math.trunc(Math.random()*6)+1
+        melody = melody + " "+ melodyScale[noteNumber]
+        console.log(noteNumber)
+    }
+    console.log(melody)
+
+    document.querySelector('.display--melody').textContent = melody
+
+    melody = ""
+    melodyScale = []
+})
+
